2e6e9122b938df198bcdbbc681e954fef07570df
Rafael Pillar <rafael.baptista@injunior.com.br>
2023-01-07 20:33:54 -0300

    feat:initial folder struct

---
+++ BackEnd/routes/index.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { usersRoutes } from "./users.routes";
+
+const router = Router();
+
+router.use("/users",usersRoutes);
---
+++ BackEnd/routes/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+
+const usersRoutes = Router();
+
+// usersRoutes.post('/', (request, response) => {
+//     return 
+// })
+
+
+
+export {usersRoutes};
